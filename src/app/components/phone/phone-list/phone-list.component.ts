db9c07d4ac3007c2edef7b01fa9edd9fc5602672
Julio García Pérez <juliojose.garcia@11paths.com>
2018-03-13 20:37:34 +0100

    feat: add auth

--- src/app/components/phone/phone-list/phone-list.component.ts
+++ src/app/components/phone/phone-list/phone-list.component.ts
@@ -1,3 +1,4 @@
+import { SessionService } from './../../../shared/services/session.service';
 import { PhonesService } from './../../../shared/services/phones.service';
 import { Phone } from './../../../shared/model/phone.model';
 import { Component, OnInit } from '@angular/core';
@@ -10,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class PhoneListComponent implements OnInit {
   phones: Array<Phone> = [];
 
-  constructor(private phonesService: PhonesService) { }
+  constructor(private phonesService: PhonesService, private sessionService: SessionService) { }
 
   ngOnInit() {
     this.phonesService.list()
